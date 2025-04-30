2806e3da6fae31c689e98bb2c77ddf908334ad03
eng.7montaser <75915101+Montaser778@users.noreply.github.com>
2025-04-30 10:47:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: [" Blogger", " Youtuber"," Student", " Programmer", " Developer", " Freelancer", " Software Engineer", "AI / ML Engineer"],
+    strings: ["AI / ML Engineer", " Blogger", " Youtuber"," Student", " Programmer", " Developer", " Freelancer", " Software Engineer"],
     typeSpeed: 100,
     backSpeed: 70,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: [" Blogger", " Youtuber"," Student", " Programmer", " Developer", " Freelancer", " Software Engineer", "AI / ML Engineer"],
+    strings: ["AI / ML Engineer", " Blogger", " Youtuber"," Student", " Programmer", " Developer", " Freelancer", " Software Engineer"],
     typeSpeed: 100,
     backSpeed: 70,
     loop: true,
